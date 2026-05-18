73e0a863772e493e2114520cbe40157829f732c4
Dmytro Farbun <dimofarb@gmail.com>
2026-05-18 12:42:27 +0300

    feat(db): integrate Prisma with PostgreSQL adapter and update database connection setup

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,5 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+import dotenv from "dotenv";
+import path from "path";
 
-const prisma = new PrismaClient();
+dotenv.config({ path: path.resolve(process.cwd(), ".env") });
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is missing!");
+}
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
